75d2a3bc206f36a5a3abea266c77e55f13afa712
harshkr101 <harsh.mca19.du@gmail.com>
2020-11-30 20:49:45 +0530

    updated project

---
+++ WebContent/common/footer.jsp
@@ -0,0 +1,16 @@
+<style>
+.footer {
+    position: fixed;
+    bottom: 0;
+    width:100%;
+    height: 40px;
+    background-color: skyblue;
+}
+
+</style>
+
+<footer class="footer font-small black">
+    <!-- Copyright -->
+  <div class="footer-copyright text-center py-3" style="color: white">© 2020 Copyright </div>
+</footer>
+<!-- Footer -->
---
+++ src/webtech/todoapp/dao/TodoDao.java
@@ -0,0 +1,20 @@
+package webtech.todoapp.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import webtech.todoapp.model.Todo;
+
+public interface TodoDao {
+
+	void insertTodo(Todo todo) throws SQLException;
+
+	Todo selectTodo(long todoId);
+
+	List<Todo> selectAllTodos();
+
+	boolean deleteTodo(int id) throws SQLException;
+
+	boolean updateTodo(Todo todo) throws SQLException;
+
+}
